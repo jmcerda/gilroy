48f74572e37089e7bc4f6464f52372a22b32b983
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-03-17 01:01:23 -0700

    webform redhen

---
+++ sites/all/modules/contrib/webform_redhen/webform_redhen.api.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file
+ * Hooks provided by the Webform RedHen module.
+ */
+
+/**
+ * Act on the contact save from webform submission.
+ *
+ * @param $node webform node
+ * @param $submission webform submission
+ * @param $contact_id RedHen contact id of the contact just saved
+ *
+ * @return void
+ */
+function hook_webform_redhen_contact_save($node, $submission, $contact_id) {
+
+}
