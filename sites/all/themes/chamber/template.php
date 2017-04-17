992aa6cbac3863b9c8b008b2551a8e2e5984e748
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-04-17 15:37:16 -0700

    .

--- sites/all/themes/chamber/template.php
+++ sites/all/themes/chamber/template.php
@@ -11,10 +11,10 @@ function chamber_preprocess_html(&$variables) {
  */
 function chamber_preprocess_page(&$variables) {
     // Get the entire main menu tree
-    $main_menu_tree = menu_tree_all_data('main-menu');
+    $main_menu_tree = menu_tree_all_data('main-menu', 1);
 
     // Add the rendered output to the $main_menu_expanded variable
-    $variables['main_menu_expanded'] = menu_tree_output($main_menu_tree, 1);
+    $variables['main_menu_expanded'] = menu_tree_output($main_menu_tree);
 }
 
 /**
