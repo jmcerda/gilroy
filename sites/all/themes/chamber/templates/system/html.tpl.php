630bf0fafdd28084cc45d2729b6b86bed04ff342
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-04-23 02:42:23 -0700

    .

--- sites/all/themes/chamber/templates/system/html.tpl.php
+++ sites/all/themes/chamber/templates/system/html.tpl.php
@@ -61,6 +61,7 @@
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
+    <?php print $scripts; ?>
   <!-- HTML5 element support for IE6-8 -->
   <!--[if lt IE 9]>
     <script src="https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv-printshiv.min.js"></script>
@@ -92,6 +93,5 @@
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
-  <?php print $scripts; ?>
 </body>
 </html>
