2dba56c1f3a0b43ff1cc111c7da79f546c9df1b8
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-04-23 20:54:40 -0700

    .

--- sites/all/themes/chamber/templates/system/page.tpl.php
+++ sites/all/themes/chamber/templates/system/page.tpl.php
@@ -74,7 +74,7 @@
  */
 ?>
 <header id="navbar" role="banner">
-  <div class="container">
+  <div>
     <div class="navbar-header">
       <?php if ($logo): ?>
         <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
