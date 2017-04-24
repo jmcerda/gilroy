a18ea6b83ad51e44d579ec6a37283c2accd4657d
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-04-23 18:48:22 -0700

    template function

--- sites/all/themes/chamber/template.php
+++ sites/all/themes/chamber/template.php
@@ -3,3 +3,26 @@
  * @file
  * The primary PHP file for this theme.
  */
+
+/**
+ * Implements template_preprocess_html().
+ */
+function chamber_preprocess_html(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_page.
+ */
+function chamber_preprocess_page(&$variables) {
+    // Get the entire main menu tree
+    $main_menu_tree = menu_tree_all_data('main-menu', NULL, 2 );
+
+    // Add the rendered output to the $main_menu_expanded variable
+    $variables['main_menu_expanded'] = menu_tree_output($main_menu_tree);
+}
+
+/**
+ * Implements template_preprocess_node.
+ */
+function chamber_preprocess_node(&$variables) {
+}
