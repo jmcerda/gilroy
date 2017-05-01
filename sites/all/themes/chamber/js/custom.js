254f9517491a8eb7643a8323e87fc2ffdc83242f
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-04-30 20:22:21 -0700

    coverflow debug

--- sites/all/themes/chamber/js/custom.js
+++ sites/all/themes/chamber/js/custom.js
@@ -42,7 +42,7 @@
                 $('html, body').animate({scrollTop : 0},800);
                 return false;
             });
-            $('.chamber-flipster').coverflow();
+            $('#coverflow').coverflow();
             // $('.chamber-flipster').flipster({
             //     itemContainer: 'div',
             //     // [string|object]
