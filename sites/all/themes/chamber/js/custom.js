a31b599277f9b9f41e646506543fb404b9337bad
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-04-30 21:40:39 -0700

    .

--- sites/all/themes/chamber/js/custom.js
+++ sites/all/themes/chamber/js/custom.js
@@ -42,7 +42,7 @@
                 $('html, body').animate({scrollTop : 0},800);
                 return false;
             });
-            $('#coverflow').coverflow();
+            $('.coverflow').coverflow();
             // $('.chamber-flipster').flipster({
             //     itemContainer: 'div',
             //     // [string|object]
