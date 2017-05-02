9226b7639ccacd4a8536f3e734b3f782a1c7db29
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-05-02 16:42:31 -0700

    tpl fix

--- sites/all/themes/chamber/templates/views/events/views-view-unformatted--events--block.tpl.php
+++ sites/all/themes/chamber/templates/views/events/views-view-unformatted--events--block.tpl.php
@@ -7,10 +7,14 @@
  * @ingroup views_templates
  */
 ?>
-
-<span class="block-icons col-sm-2 col-xs-12"><img src="sites/all/themes/chamber/images/calendar.svg"></span>
-<span class="col-sm-10 col-xs-12"><h3>UPCOMING EVENTS</h3></span>
-
+<div class="col-xs-12">
+    <span class="block-icons col-sm-2 col-xs-12">
+        <img src="sites/all/themes/chamber/images/calendar.svg">
+    </span>
+    <span class="block-title col-sm-10 col-xs-12">
+        <h3>UPCOMING EVENTS</h3>
+    </span>
+</div>
 <?php foreach ($rows as $id => $row): ?>
   <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
     <?php print $row; ?>
