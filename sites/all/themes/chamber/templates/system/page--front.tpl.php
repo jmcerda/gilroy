5a870a805958b33e49e3754eba81c88279fd40f8
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-05-02 17:59:16 -0700

    page container

--- sites/all/themes/chamber/templates/system/page--front.tpl.php
+++ sites/all/themes/chamber/templates/system/page--front.tpl.php
@@ -175,28 +175,24 @@
               </div>
           </div>
           <div id="fp-mid-nav">
-              <div class="container">
-                  <div class="row">
-                      <div class="utilities">
-                          <div class="col-sm-3 col-xs-12 utility-item directory-link equalHeight">
-                              <a href="directory">
-                                  <img src="sites/all/themes/chamber/images/magnifying_glass_wh.svg">
-                                  <span>SEARCH BUSINESS DIRECTORY</span>
-                              </a>
-                          </div>
-                          <div class="col-sm-3 col-xs-12 utility-item membership-link equalHeight">
-                              <a href="about/membership">
-                                  <img src="sites/all/themes/chamber/images/member_wh.svg">
-                                  <span>BECOME A MEMBER</span>
-                              </a>
-                          </div>
-                          <div class="col-sm-3 col-xs-12 utility-item gallery-link equalHeight">
-                              <a href="events/gallery">
-                                  <img src="sites/all/themes/chamber/images/camera_wh.svg">
-                                  <span>VIEW PHOTO GALLERY</span>
-                              </a>
-                          </div>
-                      </div>
+              <div class="utilities">
+                  <div class="col-sm-3 col-xs-12 utility-item directory-link equalHeight">
+                      <a href="directory">
+                          <img src="sites/all/themes/chamber/images/magnifying_glass_wh.svg">
+                          <span>SEARCH BUSINESS DIRECTORY</span>
+                      </a>
+                  </div>
+                  <div class="col-sm-3 col-xs-12 utility-item membership-link equalHeight">
+                      <a href="about/membership">
+                          <img src="sites/all/themes/chamber/images/member_wh.svg">
+                          <span>BECOME A MEMBER</span>
+                      </a>
+                  </div>
+                  <div class="col-sm-3 col-xs-12 utility-item gallery-link equalHeight">
+                      <a href="events/gallery">
+                          <img src="sites/all/themes/chamber/images/camera_wh.svg">
+                          <span>VIEW PHOTO GALLERY</span>
+                      </a>
                   </div>
               </div>
           </div>
