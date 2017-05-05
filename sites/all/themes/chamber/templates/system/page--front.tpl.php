cf296413bb293ca38b47a645a4a9737abb3dd878
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-05-05 14:26:52 -0700

    .

--- sites/all/themes/chamber/templates/system/page--front.tpl.php
+++ sites/all/themes/chamber/templates/system/page--front.tpl.php
@@ -227,7 +227,7 @@
 <div id="partners-nav">
     <div class="container">
         <div class="row">
-            <div id="divWheel" data-wheelnav data-wheelnav-titlewidth="50" data-wheelnav-titleheight="50" data-wheelnav-wheelradius="100" data-wheelnav-navangle="90" data-wheelnav-colors="#005a80" data-wheelnav-rotateoff>
+            <div id="divWheel" data-wheelnav>
                 <div data-wheelnav-navitemimg="../images/GilroyDowntownBusinessAssoc.svg" data-wheelnav-navitemtext="Downtown Gilroy Business Association. A vibrant and historic downtown community"><a href="www.google.com"></a></div>
                 <div data-wheelnav-navitemimg="../images/CityofGilroy.svg" data-wheelnav-navitemtext="City of Gilroy. Business licenses, bid opportunities, city services, and more"><a href="www.google.com"></a></div>
                 <div data-wheelnav-navitemimg="../images/VisitGilroy.svg" data-wheelnav-navitemtext="Visit Gilroy. Everything you need to plan a getaway to Gilroy"><a href="www.google.com"></a></div>
