79160c87df7875030af28730626ce5445b4de192
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-05-05 15:50:12 -0700

    .

--- sites/all/themes/chamber/templates/system/page--front.tpl.php
+++ sites/all/themes/chamber/templates/system/page--front.tpl.php
@@ -227,11 +227,21 @@
 <div id="partners-nav">
     <div class="container">
         <div class="row">
-            <div id="divWheel" data-wheelnav>
-                <div data-wheelnav-navitemimg="../images/GilroyDowntownBusinessAssoc.svg" data-wheelnav-navitemtext="Downtown Gilroy Business Association. A vibrant and historic downtown community"><a href="www.google.com"></a></div>
-                <div data-wheelnav-navitemimg="../images/CityofGilroy.svg" data-wheelnav-navitemtext="City of Gilroy. Business licenses, bid opportunities, city services, and more"><a href="www.google.com"></a></div>
-                <div data-wheelnav-navitemimg="../images/VisitGilroy.svg" data-wheelnav-navitemtext="Visit Gilroy. Everything you need to plan a getaway to Gilroy"><a href="www.google.com"></a></div>
-                <div data-wheelnav-navitemimg="../images/GilroyEconomicDevelopmentCorp.png" data-wheelnav-navitemtext="Gilroy Economic Development Corporation Resources to help you open, expand or relocate your business"><a href="www.google.com"></a></div>
+            <div class="col-xs-12">
+                <div id="divWheel" data-wheelnav data-wheelnav-colors="#005a80">
+                    <div data-wheelnav-navitemimg="../images/GilroyDowntownBusinessAssoc.svg" data-wheelnav-navitemtext="Downtown Gilroy Business Association. A vibrant and historic downtown community">
+                        <a href="www.google.com"></a>
+                    </div>
+                    <div data-wheelnav-navitemimg="../images/CityofGilroy.svg" data-wheelnav-navitemtext="City of Gilroy. Business licenses, bid opportunities, city services, and more">
+                        <a href="www.google.com"></a>
+                    </div>
+                    <div data-wheelnav-navitemimg="../images/VisitGilroy.svg" data-wheelnav-navitemtext="Visit Gilroy. Everything you need to plan a getaway to Gilroy">
+                        <a href="www.google.com"></a>
+                    </div>
+                    <div data-wheelnav-navitemimg="../images/GilroyEconomicDevelopmentCorp.png" data-wheelnav-navitemtext="Gilroy Economic Development Corporation Resources to help you open, expand or relocate your business">
+                        <a href="www.google.com"></a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
