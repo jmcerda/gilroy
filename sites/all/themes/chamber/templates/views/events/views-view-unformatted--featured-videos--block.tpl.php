0d8958985de293f0641568b2128336a148ffae2e
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-05-04 19:41:13 -0700

    events text divs

--- sites/all/themes/chamber/templates/views/events/views-view-unformatted--featured-videos--block.tpl.php
+++ sites/all/themes/chamber/templates/views/events/views-view-unformatted--featured-videos--block.tpl.php
@@ -8,15 +8,18 @@
  */
 ?>
 <div class="col-xs-12">
-    <span class="block-icons col-sm-2 col-xs-12">
-        <img src="sites/all/themes/chamber/images/weeklyvideo.svg">
-    </span>
-    <span class="col-sm-10 col-xs-12">
-        <h3>WEEKLY VIDEO</h3>
-    </span>
+    <div>
+        <span class="block-icons col-sm-2 col-xs-12">
+            <img src="sites/all/themes/chamber/images/weeklyvideo.svg">
+        </span>
+        <span class="col-sm-10 col-xs-12">
+            <h3>WEEKLY VIDEO</h3>
+        </span>
+    </div>
+    <?php foreach ($rows as $id => $row): ?>
+        <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
+            <?php print $row; ?>
+        </div>
+    <?php endforeach; ?>
 </div>
-<?php foreach ($rows as $id => $row): ?>
-  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
-    <?php print $row; ?>
-  </div>
-<?php endforeach; ?>
+
