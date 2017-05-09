6aa002dd5e736c2391e0d5edf90db91906d7fc1b
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-05-08 22:26:10 -0700

    flipster

--- sites/all/themes/chamber/js/custom.js
+++ sites/all/themes/chamber/js/custom.js
@@ -50,7 +50,7 @@
             //     index: 5,
             // });
             $('.chamber-flipster').flipster({
-                itemContainer: 'div',
+                itemContainer: '.flipster__container',
                 // [string|object]
                 // Selector for the container of the flippin' items.
 
