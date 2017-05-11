d45859512b26df5b43a569d1e9a9a2f5223d415a
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-05-11 00:42:17 -0700

    wheel

--- sites/all/themes/chamber/js/custom.js
+++ sites/all/themes/chamber/js/custom.js
@@ -136,7 +136,7 @@
             // myWheelnav.createWheel([icon.smile, icon.star, icon.fork, icon.$]);
 
             // Ad slider wheel
-            $('.ad-wheel').flipster({
+            $('.ad-slider-block').flipster({
                 style: 'wheel',
                 spacing: 0
             });
