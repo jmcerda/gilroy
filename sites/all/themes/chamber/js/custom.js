9361c698a23732810d80134493602ad44117abb3
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-05-11 20:56:01 -0700

    .

--- sites/all/themes/chamber/js/custom.js
+++ sites/all/themes/chamber/js/custom.js
@@ -10,6 +10,8 @@
             //     'padding': 256,
             //     'tolerance': 70
             // });
+
+            // Page hero placements.
             $(".pane-bundle-page-hero").appendTo(".page-hero");
             // Equal heights
             $('.equalHeight').matchHeight({
@@ -28,6 +30,7 @@
             // });
 
             //Scroll to top
+
             //Check to see if the window is top if not then display button
             $(window).scroll(function(){
                 if ($(this).scrollTop() > 100) {
@@ -43,12 +46,6 @@
                 return false;
             });
 
-
-            // $('.coverflow').coverflow({
-            //     duration: 'slow',
-            //     density: 5,
-            //     index: 5,
-            // });
             $('.chamber-flipster').flipster({
                 itemContainer: '.view-content',
                 // [string|object]
@@ -127,78 +124,70 @@
                 // Arguments received: [currentItem, previousItem]
             });
 
-            // Wheelnav
-            // var wheel = new wheelnav("divWheel");
-            // var wheel.wheelRadius = 400;
-            // var myWheelnav = new wheelnav("divWheelnav");
-            // myWheelnav.slicePathFunction = slicePath().WheelSlice;
-            // myWheelnav.colors = colorpalette.parrot;
-            // myWheelnav.createWheel([icon.smile, icon.star, icon.fork, icon.$]);
-
             // Ad slider wheel
-            $('.chamber-ad-slider').flipster({
-                style: 'wheel',
-                spacing: 0,
-                itemContainer: '.view-content',
-                itemSelector: '.ad-slider-slide',
-                start: 1,
-                // ['center'|number]
-                // Zero based index of the starting item, or use 'center' to start in the middle
-
-                fadeIn: 800,
-                // [milliseconds]
-                // Speed of the fade in animation after items have been setup
-
-                loop: true,
-
-                autoplay: 10000,
-                // [false|milliseconds]
-                // If a positive number, Flipster will automatically advance to next item after that number of milliseconds
-
-                pauseOnHover: true,
-                // [true|false]
-                // If true, autoplay advancement will pause when Flipster is hovered
-
-                click: true,
-                // [true|false]
-                // Clicking an item switches to that item
-
-                keyboard: true,
-                // [true|false]
-                // Enable left/right arrow navigation
-
-                scrollwheel: true,
-                // [true|false]
-                // Enable mousewheel/trackpad navigation; up/left = previous, down/right = next
-
-                touch: true,
-                // [true|false]
-                // Enable swipe navigation for touch devices
-
-                nav: false,
-                // [true|false|'before'|'after']
-                // If not false, Flipster will build an unordered list of the items
-                // Values true or 'before' will insert the navigation before the items, 'after' will append the navigation after the items
-
-                buttons: true,
-                // [true|false|'custom']
-                // If true, Flipster will insert Previous / Next buttons with SVG arrows
-                // If 'custom', Flipster will not insert the arrows and will instead use the values of `buttonPrev` and `buttonNext`
-
-                buttonPrev: 'Previous',
-                // [text|html]
-                // Changes the text for the Previous button
-
-                buttonNext: 'Next',
-                // [text|html]
-                // Changes the text for the Next button
-
-                onItemSwitch: false
-                // [function]
-                // Callback function when items are switched
-                // Arguments received: [currentItem, previousItem]
-
-            });
+            // $('.chamber-ad-slider').flipster({
+            //     style: 'wheel',
+            //     spacing: 0,
+            //     itemContainer: '.view-content',
+            //     itemSelector: '.ad-slider-slide',
+            //     start: 1,
+            //     // ['center'|number]
+            //     // Zero based index of the starting item, or use 'center' to start in the middle
+            //
+            //     fadeIn: 800,
+            //     // [milliseconds]
+            //     // Speed of the fade in animation after items have been setup
+            //
+            //     loop: true,
+            //
+            //     autoplay: 10000,
+            //     // [false|milliseconds]
+            //     // If a positive number, Flipster will automatically advance to next item after that number of milliseconds
+            //
+            //     pauseOnHover: true,
+            //     // [true|false]
+            //     // If true, autoplay advancement will pause when Flipster is hovered
+            //
+            //     click: true,
+            //     // [true|false]
+            //     // Clicking an item switches to that item
+            //
+            //     keyboard: true,
+            //     // [true|false]
+            //     // Enable left/right arrow navigation
+            //
+            //     scrollwheel: true,
+            //     // [true|false]
+            //     // Enable mousewheel/trackpad navigation; up/left = previous, down/right = next
+            //
+            //     touch: true,
+            //     // [true|false]
+            //     // Enable swipe navigation for touch devices
+            //
+            //     nav: false,
+            //     // [true|false|'before'|'after']
+            //     // If not false, Flipster will build an unordered list of the items
+            //     // Values true or 'before' will insert the navigation before the items, 'after' will append the navigation after the items
+            //
+            //     buttons: true,
+            //     // [true|false|'custom']
+            //     // If true, Flipster will insert Previous / Next buttons with SVG arrows
+            //     // If 'custom', Flipster will not insert the arrows and will instead use the values of `buttonPrev` and `buttonNext`
+            //
+            //     buttonPrev: 'Previous',
+            //     // [text|html]
+            //     // Changes the text for the Previous button
+            //
+            //     buttonNext: 'Next',
+            //     // [text|html]
+            //     // Changes the text for the Next button
+            //
+            //     onItemSwitch: false
+            //     // [function]
+            //     // Callback function when items are switched
+            //     // Arguments received: [currentItem, previousItem]
+            //
+            // });
 
             // SVG img replace
             $('img[src$=".svg"]').each(function() {
