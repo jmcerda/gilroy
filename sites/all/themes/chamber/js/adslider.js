fbd86afec940138e26c665dd0aedb13dd7ad2502
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-05-12 12:42:42 -0700

    slider js in own file

---
+++ sites/all/themes/chamber/js/adslider.js
@@ -0,0 +1,16 @@
+/**
+ * Created by jcerda on 5/12/17.
+ */
+var upClass = 'toggle-up';
+var downClass = 'toggle-down';
+
+function toggle() {
+    var square = document.querySelector('.square');
+
+    if (~square.className.indexOf(downClass)) {
+        square.className = square.className.replace(downClass, upClass);
+    } else {
+        square.className = square.className.replace(upClass, downClass);
+    }
+
+}
