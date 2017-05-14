62e47e47e728968d2b118a01d146d65d9735549f
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-05-14 02:10:14 -0700

    click

--- sites/all/themes/chamber/js/fphero.js
+++ sites/all/themes/chamber/js/fphero.js
@@ -5,15 +5,15 @@
     Drupal.behaviors.fpheroModule = {
         attach: function (context, settings) {
             // Front page slider flip function
-            // $(".card").click(function() {
-            //     $(this).addClass("flipped");
-            // });
-            //
-            $(".card").click(function(){
-                if ($(".card .flipster-slide").hasClass('flipster__item--current')) {
-                    $(this).closest().addClass('flipped');
-                }
+            $(".card").click(function() {
+                $(this).addClass("flipped");
             });
+            //
+            // $(".card").click(function(){
+            //     if ($(".card .flipster-slide").hasClass('flipster__item--current')) {
+            //         $(this).closest().addClass('flipped');
+            //     }
+            // });
             // $(".card").click(function() {
             //     $(".flipster__item--current").closest(".card").addClass("flipped");
             // });
