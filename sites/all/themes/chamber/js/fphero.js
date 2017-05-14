c2ac19a6f3267e4920d91b9536a4d0894b82868c
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-05-14 00:35:01 -0700

    .

--- sites/all/themes/chamber/js/fphero.js
+++ sites/all/themes/chamber/js/fphero.js
@@ -6,11 +6,10 @@
         attach: function (context, settings) {
             // Front page slider flip function
             $(".card").click(function() {
-                // $(this).addClass("flipped");
-                if ( $(this).hasClass('flipped') ) {
-                    $(this).removeClass('flipped');
-                }
-                $(this).addClass('current');
+                $(this).addClass("flipped");
+            });
+            $(".card").blur( function(){
+                $(this).removeClass("flipped");
             });
         }
     };
