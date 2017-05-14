c57c23c78fa748813308899259b950a52046c626
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-05-14 00:14:38 -0700

    new js

--- sites/all/themes/chamber/js/fphero.js
+++ sites/all/themes/chamber/js/fphero.js
@@ -1,12 +1,13 @@
 /**
  * Created by jcerda on 5/13/17.
  */
-(function flip($) {
-    Drupal.behaviors.exampleModule = {
-        attach: function flip() {
-            // Code to be run on page load, and
-            // on ajax load added here
-            $('.card').toggleClass('flipped');
+(function ($) {
+    Drupal.behaviors.fpheroModule = {
+        attach: function (context, settings) {
+            // Front page slider flip function
+            $(".card").click(function() {  //use a class, since your ID gets mangled
+                $(this).addClass("flipped");      //add the class to the clicked element
+            });
         }
     };
 }(jQuery));
