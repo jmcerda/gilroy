b8357795a1e496b6a875dd6b8d43bc3349984fd6
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-05-16 00:32:42 -0700

    js reorder

--- sites/all/themes/chamber/js/custom.js
+++ sites/all/themes/chamber/js/custom.js
@@ -221,6 +221,31 @@
             $('.dropdown').on('hide.bs.dropdown', function() {
                 $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
             });
+
+            // Google map stuff
+            var onMapMouseleaveHandler = function (event) {
+                var that = $(this);
+
+                that.on('click', onMapClickHandler);
+                that.off('mouseleave', onMapMouseleaveHandler);
+                that.find('#map').css("pointer-events", "none");
+            }
+
+            var onMapClickHandler = function (event) {
+                var that = $(this);
+
+                // Disable the click handler until the user leaves the map area
+                that.off('click', onMapClickHandler);
+
+                // Enable scrolling zoom
+                that.find('#map').css("pointer-events", "auto");
+
+                // Handle the mouse leave event
+                that.on('mouseleave', onMapMouseleaveHandler);
+            }
+
+            // Enable map zooming with mouse scroll when the user clicks the map
+            $('.maps.embed-container').on('click', onMapClickHandler);
         }
     };
 })(jQuery);
