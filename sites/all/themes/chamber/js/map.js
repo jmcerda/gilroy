e7b6a0f3ffb77b27cd1cfed2111d49189423fdb3
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-05-15 23:41:50 -0700

    .

--- sites/all/themes/chamber/js/map.js
+++ sites/all/themes/chamber/js/map.js
@@ -5,7 +5,7 @@
 
                 function initMap() {
 
-                    var location = new google.maps.LatLng(37.0077292, -121.5718018);
+                    var location = new google.maps.LatLng(50.0875726, 14.4189987);
 
                     var mapCanvas = document.getElementById('map');
                     var mapOptions = {
@@ -15,6 +15,31 @@
                         mapTypeId: google.maps.MapTypeId.ROADMAP
                     }
                     var map = new google.maps.Map(mapCanvas, mapOptions);
+
+                    var markerImage = '../images/location.svg';
+
+                    var marker = new google.maps.Marker({
+                        position: location,
+                        map: map,
+                        icon: markerImage
+                    });
+
+                    var contentString = '<div class="info-window">' +
+                        '<h3>The Gilroy Chamber of Commerce</h3>' +
+                        '<div class="info-content">' +
+                        '<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>' +
+                        '</div>' +
+                        '</div>';
+
+                    var infowindow = new google.maps.InfoWindow({
+                        content: contentString,
+                        maxWidth: 400
+                    });
+
+                    marker.addListener('click', function () {
+                        infowindow.open(map, marker);
+                    });
+
 
                 }
 
