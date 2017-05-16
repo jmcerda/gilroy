a0730632f9ae9e0b53ab72c2ef59406c7963a5b3
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-05-15 21:59:16 -0700

    mapping header

--- sites/all/themes/chamber/js/map.js
+++ sites/all/themes/chamber/js/map.js
@@ -1,11 +1,31 @@
 /**
  * Created by jcerda on 5/15/17.
  */
-function gilroyMap() {
-    var mapOptions = {
-        center: new google.maps.LatLng(51.5, -0.12),
-        zoom: 10,
-        mapTypeId: google.maps.MapTypeId.HYBRID
+// function gilroyMap() {
+//     var mapOptions = {
+//         center: new google.maps.LatLng(51.5, -0.12),
+//         zoom: 10,
+//         mapTypeId: google.maps.MapTypeId.HYBRID
+//     }
+//     var map = new google.maps.Map(document.getElementById("gilroymap"), mapOptions);
+// }
+
+$(function () {
+
+    function initMap() {
+
+        var location = new google.maps.LatLng(37.0077292, -121.5718018);
+
+        var mapCanvas = document.getElementById('gilroymap');
+        var mapOptions = {
+            center: location,
+            zoom: 16,
+            panControl: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(mapCanvas, mapOptions);
+
     }
-    var map = new google.maps.Map(document.getElementById("gilroymap"), mapOptions);
-}
+
+    google.maps.event.addDomListener(window, 'load', initMap);
+});
