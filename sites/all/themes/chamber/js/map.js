0c859ef7cdc851709a7ca04fdba2200a7caf6671
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-05-15 23:46:47 -0700

    js

--- sites/all/themes/chamber/js/map.js
+++ sites/all/themes/chamber/js/map.js
@@ -5,7 +5,7 @@
 
                 function initMap() {
 
-                    var location = new google.maps.LatLng(50.0875726, 14.4189987);
+                    var location = new google.maps.LatLng(37.0077292, -121.5718018);
 
                     var mapCanvas = document.getElementById('map');
                     var mapOptions = {
@@ -16,7 +16,7 @@
                     }
                     var map = new google.maps.Map(mapCanvas, mapOptions);
 
-                    var markerImage = '../images/location.svg';
+                    var markerImage = 'sites/all/themes/chamber/images/location.svg';
 
                     var marker = new google.maps.Marker({
                         position: location,
