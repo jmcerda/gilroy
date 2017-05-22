947f4f30f393e551ea26c846db2e6580a707b9b2
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-05-21 19:07:25 -0700

    intervalset

--- sites/all/themes/chamber/js/adslider.js
+++ sites/all/themes/chamber/js/adslider.js
@@ -4,14 +4,12 @@
 var upClass = 'toggle-up';
 var downClass = 'toggle-down';
 
-setInterval(function(){
-    function toggle() {
-        var square = document.querySelector('.square');
+setInterval(function toggle() {
+    var square = document.querySelector('.square');
 
-        if (~square.className.indexOf(downClass)) {
-            square.className = square.className.replace(downClass, upClass);
-        } else {
-            square.className = square.className.replace(upClass, downClass);
-        }
+    if (~square.className.indexOf(downClass)) {
+        square.className = square.className.replace(downClass, upClass);
+    } else {
+        square.className = square.className.replace(upClass, downClass);
     }
 }, 5000);
