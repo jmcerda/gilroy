a239cabad5e3568ac2aa0f7e7249fa800d4a5c05
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-05-22 18:45:49 -0700

    2col tpl

---
+++ sites/all/themes/chamber/templates/panels/bootstrap-twocol-stacked.tpl.php
@@ -0,0 +1,22 @@
+<div class="<?php print $classes ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+  <?php if ($content['top']): ?>
+      <div class="row">
+        <?php print $content['top']; ?>
+      </div>
+  <?php endif ?>
+  <?php if ($content['left'] || $content['right']): ?>
+      <div class="row">
+          <div class="col-sm-6" role="complementary">
+            <?php print $content['left']; ?>
+          </div>
+          <div class="col-sm-6" role="complementary">
+            <?php print $content['right']; ?>
+          </div>
+      </div>
+  <?php endif ?>
+  <?php if ($content['bottom']): ?>
+      <div id="page-bottom" class="row">
+        <?php print $content['bottom']; ?>
+      </div>
+  <?php endif ?>
+</div>
