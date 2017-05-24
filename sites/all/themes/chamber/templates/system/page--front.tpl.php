ee61cf546522ce7508c56e2e97e959ba3167648b
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-05-23 19:58:58 -0700

    parallex front work

--- sites/all/themes/chamber/templates/system/page--front.tpl.php
+++ sites/all/themes/chamber/templates/system/page--front.tpl.php
@@ -230,8 +230,6 @@
 </div>
 <div id="partners-nav">
     <div class="parallax">
-        <div class="parallax__layer parallax__layer--back"></div>
-        <div class="parallax__layer parallax__layer--base"></div>
         <div class="container">
             <div class="row">
                 <div id="fp-chamber-nav">
@@ -283,6 +281,8 @@
                 </div>
             </div>
         </div>
+        <div class="parallax__layer parallax__layer--back"></div>
+        <div class="parallax__layer parallax__layer--base"></div>
     </div>
 </div>
 <div id="fp-ads-slider">
